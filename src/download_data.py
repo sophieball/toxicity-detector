97542433d32dfbc347de20fb50691253c64e6a26
Sophie Qiu <hsqq@google.com>
2020-06-16 02:54:05 +0000

    remove download spacy

--- src/download_data.py
+++ src/download_data.py
@@ -27,8 +27,3 @@ def download_data():
     spacy.load("en")
   except IOError:
     spacy.cli.download("en")
-
-  try:
-    spacy.load("en_core_web_md")
-  except IOError:
-    spacy.cli.download("en_core_web_md")
