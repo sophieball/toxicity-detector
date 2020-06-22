f1d924c4e96b92f8db5374a624312f4cf54b0d17
Sophie Qiu <hsqq@google.com>
2020-06-22 21:56:27 +0000

    added is_SE and self_angry columns to output; outputs a csv of different politeness strategies

--- src/convo_politeness.py
+++ src/convo_politeness.py
@@ -1,13 +1,12 @@
 # Lint as: python3
-"""
-Use ConvoKit to get politeness score
-"""
+"""Use ConvoKit to get politeness score"""
 
 from convokit import Corpus, Speaker, Utterance
 from collections import defaultdict
 from convokit.text_processing import TextParser
 from convokit import PolitenessStrategies
 import pandas as pd
+
 
 # Creating corpus from the list of utterances
 def prepare_corpus(comments):
@@ -28,6 +27,7 @@ def prepare_corpus(comments):
   corpus = Corpus(utterances=utterance_list)
   return corpus
 
+
 # input: a pandas dataframe: _id, text
 # output: a pandas dataframe: _id, text, stanford_politeness
 def get_politeness_score(comments):
@@ -37,14 +37,27 @@ def get_politeness_score(comments):
   corpus = parser.transform(corpus)
   ps = PolitenessStrategies()
   corpus = ps.transform(corpus, markers=True)
-  #query = lambda x : x.meta["politeness_strategies"]["feature_politeness_==HASPOSITIVE=="] == 1
+
   num_features = len(
       corpus.get_utterance(
           corpus.get_utterance_ids()[0]).meta["politeness_strategies"])
-  scores = [{
-      "_id":
-          x,
-      "stanford_polite":
-          sum(corpus.get_utterance(x).meta["politeness_strategies"].values()) / num_features
-  } for x in corpus.get_utterance_ids()]
+  scores = []
+  for x in corpus.get_utterance_ids():
+    ret = {"_id": x}
+    utt = corpus.get_utterance(x)
+    total = 0
+    for ((k, v), (k1, v2)) in zip(utt.meta["politeness_strategies"].items(),
+                                  utt.meta["politeness_markers"].items()):
+      cur_name = k.split("==")[1]
+      if v != 0:
+        ret[cur_name] = len(v2)
+      else:
+        ret[cur_name] = 0
+      # currently I'm taking average of strategies that are not associated with
+      # higher likelihood of being toxic
+      if cur_name != "HASNEGATIVE" and cur_name != "2nd_person" \
+          and cur_name != "2nd_person_start":
+        total += v
+    ret["stanford_polite"] = total / num_features
+    scores.append(ret)
   return pd.DataFrame(scores)
