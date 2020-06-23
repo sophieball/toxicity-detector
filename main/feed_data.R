9db05a1184e11f7f9d45bd33f95c92e2cce2ed49
Sophie Qiu <hsqq@google.com>
2020-06-23 20:47:52 +0000

    removed un-used dataframe, added comments

--- main/feed_data.R
+++ main/feed_data.R
@@ -1,15 +1,9 @@
 library(plyr)
 library(readr)
 
-df <- data.frame(
-  id = c(1,2,3,4,5),
-  text = c("great","not great", "you jerk", "bananas", "orange jerks"),
-  label = c(F, F, T, F, NA),
-  training = c(T, T, T, T, F)
-)
-getwd()
+# a csv containing BOTH Training and Testing DATA
+# 4 columns: _id, text, training (True/False), label (True/False)
 df <- read.csv("./src/data/both_t_data.csv")
-#df <- read.csv("~/Desktop/example_data.csv")
 df <- rename(df, c("X_id" = "id"))
 
 system2("../../main/train_classifier_g",
