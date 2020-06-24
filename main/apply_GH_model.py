e788431a683e6c703cd29a650553ee8edd52807f
Sophie Qiu <hsqq@google.com>
2020-06-24 23:20:37 +0000

    a test for applying GH model

--- main/apply_GH_model.py
+++ main/apply_GH_model.py
@@ -7,34 +7,41 @@ import logging
 import pickle
 
 logging.basicConfig(
-    filename="main/test_model.log", filemode="w", level=logging.INFO)
+    filename="tests/test_model.log", filemode="w", level=logging.INFO)
 logging.basicConfig(level=logging.INFO)
 
-s = suite.Suite()
 
-# process data
-[_, unlabeled_data] = receive_data.receive_data()
-s.set_unlabeled_set(unlabeled_data)
+# input: a pd.DataFrame
+def apply_model(unlabeled_data):
+  s = suite.Suite()
 
-# set model
-model_file = open("src/pickles/SVM_model.p", "rb")
-model = pickle.load(model_file)
-model_file.close()
-s.set_trained_model(model)
+  # process data
+  s.set_unlabeled_set(unlabeled_data)
 
-# select features
-s.features = ["perspective_score", "stanford_polite"]
-s.nice_features = ["perspective_score", "stanford_polite"]
+  # set model
+  model_file = open("src/pickles/SVM_model.p", "rb")
+  model = pickle.load(model_file)
+  model_file.close()
+  s.set_trained_model(model)
 
-# fit the model on test data
-result = s.test_issue_classifications_from_comments_all()
+  # select features
+  s.features = ["perspective_score", "stanford_polite"]
+  s.nice_features = ["perspective_score", "stanford_polite"]
 
-# only write the id and label to file
-if "_id" in result.columns
-  result = result.rename(columns={"_id": "id"})
-result = result.rename(columns={"stanford_polite": "politeness_score"})
-result = result[[
-    "id", "perspective_score", "politeness_score", "prediction", "is_SE",
-    "self_angry"
-]]
-result.to_csv("classification_results.csv", index=False)
+  # fit the model on test data
+  result = s.test_issue_classifications_from_comments_all()
+
+  # only write the id and label to file
+  if "_id" in result.columns:
+    result = result.rename(columns={"_id": "id"})
+  result = result.rename(columns={"stanford_polite": "politeness_score"})
+  result = result[[
+      "id", "perspective_score", "politeness_score", "prediction", "is_SE",
+      "self_angry"
+  ]]
+  result.to_csv("main/output/classification_results.csv", index=False)
+
+
+if __name__ == "__main__":
+  [_, unlabeled_data] = receive_data.receive_data()
+  apply_model(unlabeled_data)
