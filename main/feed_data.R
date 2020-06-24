3e95dc2fc971dfa81af5d704ec710d92ac64f144
Sophie Qiu <hsqq@google.com>
2020-06-24 17:09:47 +0000

    fixed typo, added paper link

--- main/feed_data.R
+++ main/feed_data.R
@@ -1,7 +1,8 @@
 library(plyr)
 library(readr)
 
-# the combined training data nad test data used in the NIER2019 paper
+# the combined training data and test data used in the NIER2019 paper:
+# https://cmustrudel.github.io/papers/raman20toxicity.pdf
 df <- read.csv("./src/data/both_t_data.csv")
 df <- rename(df, c("X_id" = "id"))
 
