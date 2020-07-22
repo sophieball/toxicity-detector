cc8e309acd12ad76c5c31b4cb3a25c7bd35ffc41
Sophie Qiu <hsqq@google.com>
2020-07-22 21:16:06 +0000

    Used Lan's code for factor analysis

---
+++ main/factor_lan.R
@@ -0,0 +1,38 @@
+# Code by Lan Cheng
+
+# redirect output to a file
+sink("factor.out")
+
+# Factor analysis to pushback metrics to 3-7 factors
+dat <- read.csv("/dev/stdin")
+
+# Use elbow method to determine the optimal number of factors. Here the elbow
+# point is determined by visual inspection
+png(paste("scree", "_", toString(format(Sys.time(), format="%y-%m-%d_%H:%M:%S")), ".png", sep=""))
+plot(eigen(cor(dat))$values)
+
+for (numFactor in 3:7){
+  fa.result <- factanal(dat,
+                        factors = numFactor,
+                        rotation = "varimax",
+                        scores = "Bartlett"
+  )
+  fa.result.loading <- fa.result$loadings
+  fa.result.scores <- data.frame(fa.result$scores)
+  fa.result.scores$fingerprint <- dat$fingerprint
+  cat(paste(numFactor, " factors:"))
+  cat(fa.result, digits=2, cutoff=0.3, sort=TRUE)
+  cat("\n\n")
+
+  # plot first 2 factors
+  load <- fa.result$loadings[,1:2]
+  png(paste("factors", numFactor, "_", toString(format(Sys.time(), format="%y-%m-%d_%H:%M:%S")), ".png", sep=""))
+  plot(load, xlab="Factor 1", ylab="Factor 2",
+       ylim=c(-1, 1), xlim=c(-1, 1), main="Varimax rotation")
+  text(
+       fa.result$loadings[,1]-0.08,
+       fa.result$loadings[,2]+0.08,
+       colnames(dat),
+       col="blue"
+     )
+}
