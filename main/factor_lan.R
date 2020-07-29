7c17016649e5ead906610957e89345ef4a6bf1a1
Sophie Qiu <hsqq@google.com>
2020-07-29 02:07:51 +0000

    remove linear comb

--- main/factor_lan.R
+++ main/factor_lan.R
@@ -6,11 +6,14 @@ sink("factor.out")
 
 # Factor analysis to pushback metrics to 3-7 factors
 dat <- read.csv("/dev/stdin")
+dat$comb <- 2*dat$mot1 + 3*dat$mot2
 
 # Check variance
-print(paste("dataframe shape:", dim(dat)))
+print("initial dataframe shape:")
+print(dim(dat))
 no_var <- as.numeric(which(apply(dat, 2, var) == 0))
-print(paste("columns with no variance:", no_var))
+print("columns with no variance:")
+print(no_var)
 # Remove columns with 0 variance
 if (length(no_var) > 0){
   dat <- dat[ - no_var]
@@ -20,8 +23,9 @@ print(paste("dataframe shape after droping no var cols:", dim(dat)))
 linear_combos <- findLinearCombos(dat)
 linear_combos
 # remove columns that are linear combos
-dat <- dat[, ! names(dat) %in% linear_combos$remove]
-print(paste("dataframe shape after dropping linear combination cols:", dim(dat)))
+dat <- dat[, - as.numeric(linear_combos$remove)]
+print("dataframe shape after dropping linear combination cols:")
+print(dim(dat))
 
 
 # Use elbow method to determine the optimal number of factors. Here the elbow
@@ -38,7 +42,7 @@ for (numFactor in 3:7){
   fa.result.loading <- fa.result$loadings
   fa.result.scores <- data.frame(fa.result$scores)
   fa.result.scores$fingerprint <- dat$fingerprint
-  print(paste(numFactor, " factors:"))
+  print(paste("Trying ", numFactor, " factors:"))
   print(fa.result, digits=2, cutoff=0.3, sort=TRUE)
   cat("\n\n")
 
