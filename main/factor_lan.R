3de346bbb5fbacb5447ff0ed61cd7f8486970920
Sophie Qiu <hsqq@google.com>
2020-08-22 02:37:51 +0000

    almost finished. need to figure out how to interpret the result. My account may expire any time, so I'll push it before I lose access

--- main/factor_lan.R
+++ main/factor_lan.R
@@ -1,4 +1,7 @@
+#https://journal.r-project.org/archive/2016/RJ-2016-021/RJ-2016-021.pdf
+
 # Code by Lan Cheng
+library(mclust)
 library(caret)
 
 # redirect output to a file
@@ -6,6 +9,7 @@ sink("factor.out")
 
 # Factor analysis to pushback metrics to 3-7 factors
 dat <- read.csv("/dev/stdin")
+dat[is.na(dat)] <- 0
 
 # Check variance
 print("initial dataframe shape:")
@@ -22,15 +26,15 @@ print("dataframe shape after droping no var cols:")
 print(dim(dat))
 
 # find linear combinations in dat
-linear_combos <- findLinearCombos(dat)
-cat("\n\n")
-print("Columns that are linear combos:")
-print(linear_combos)
+#linear_combos <- findLinearCombos(dat)
+#cat("\n\n")
+#print("Columns that are linear combos:")
+#print(linear_combos)
 
 # remove columns that are linear combos
-if (length(linear_combos$remove) > 0){
-  dat <- dat[, - as.numeric(linear_combos$remove)]
-}
+#if (length(linear_combos$remove) > 0){
+#  dat <- dat[, - as.numeric(linear_combos$remove)]
+#}
 print("dataframe shape after checking linear combination cols:")
 print(dim(dat))
 
@@ -38,9 +42,9 @@ print(dim(dat))
 # Use elbow method to determine the optimal number of factors. Here the elbow
 # point is determined by visual inspection
 png(paste("scree", "_", toString(format(Sys.time(), format="%y-%m-%d_%H:%M:%S")), ".png", sep=""))
-plot(eigen(cor(dat))$values)
+#plot(eigen(cor(dat))$values)
 
-for (numFactor in 3:7){
+for (numFactor in 3:4){
   fa.result <- factanal(dat,
                         factors = numFactor,
                         rotation = "varimax",
@@ -53,18 +57,26 @@ for (numFactor in 3:7){
   print(paste("Trying ", numFactor, " factors:"))
   print(fa.result, digits=2, cutoff=0.3, sort=TRUE)
 
-  # plot first 2 factors
-  load <- fa.result$loadings[,1:2]
-  png(paste("factors", numFactor, "_", toString(format(Sys.time(), format="%y-%m-%d_%H:%M:%S")), ".png", sep=""))
-  plot(load, xlab="Factor 1", ylab="Factor 2",
-       ylim=c(-1, 1), xlim=c(-1, 1), main="Varimax rotation")
-  text(
-       fa.result$loadings[,1]-0.08,
-       fa.result$loadings[,2]+0.08,
-       colnames(dat),
-       col="blue"
-     )
+  # Clustering analysis using GMM model
+  # Determine the optimal number of clusters
+  dataModel <- fa.result.scores#[, -which(colnames(fa.result.scores) %in% c("fingerprint"))]
+  # The range of number of clusters is set between 1 and 30. Results with more
+  # than 30 clusters are not meaningful.
+  gMin <- 1
+  gMax <- 30
+  # Calculate BIC curve, using VVV model
+  set.seed(10086)
+  BIC <- mclustBIC(dataModel, G = gMin:gMax, modelNames = "VVV")
+  plot(BIC)
+  # Use GMM clustering model with optimal number of clusters
+  set.seed(10086)
+  optimalMod <- Mclust(dataModel, x = BIC)
+  print(summary(optimalMod))
+  clusterResult <- dataModel
+  print(clusterResult)
 }
+
+
 
 write("Done.", stderr())
 write(paste("\nPlots are stored in path `",
