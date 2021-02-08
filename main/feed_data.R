d2657ce7c9a7c02c18a0ce7a208399e5d1a9c64e
sophieball <qiuhuilian1991@gmail.com>
2021-02-07 20:46:01 -0500

    change file name

--- main/feed_data.R
+++ main/feed_data.R
@@ -3,8 +3,11 @@ library(readr)
 
 # the combined training data and test data used in the NIER2019 paper:
 # https://cmustrudel.github.io/papers/raman20toxicity.pdf
+
+# Load data
 #df <- read.csv("src/data/both_t_data_thread.csv")
-df <- read.csv("src/data/pr_body_comments_unique.csv")
+df <- read.csv("src/data/pr_body_comments.csv")
+
 df <- rename(df, c("X_id" = "id"))
 
 # I leave them as comment here so I can easily switch
@@ -12,5 +15,6 @@ system2("main/train_classifier_g",
 #system2("src/find_SE_words",
 #system2("main/politeness_logi",
 #system2("src/convo_word_freq_diff",
+        args = "GH",
         stdout = "",
         input = format_csv(df))
